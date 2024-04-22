ed923ddc108f42b18f4c3ba4dc68683692f976f2
Chrisando Ryan <chrisando.siahaan@gmail.com>
2024-04-22 15:24:43 +0700

    update web-2 codebase

---
+++ web-2/models/user.model.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define('users', {
+        email: {
+            type: Sequelize.STRING,
+        },
+        username: {
+            type: Sequelize.STRING,
+        },
+        password: {
+            type: Sequelize.STRING,
+        },
+    });
+    return User;
+}
--- web-2/routes/index.js
+++ web-2/routes/index.js
@@ -1,9 +1,51 @@
 var express = require('express');
+const bcrypt = require('bcryptjs');
+
+const db = require("../models");
+const User = db.users;
+
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/login', function(req, res, next) {
+  const successMessage = req.session.successMessage;
+  const errorMessage = req.session.errorMessage;
+
+  req.session.successMessage = null;
+  req.session.errorMessage = null;
+
+  res.render('index', { successMessage, errorMessage });
+});
+
+router.get('/register', function(req, res, next) {
+  const successMessage = req.session.successMessage;
+  const errorMessage = req.session.errorMessage;
+
+  req.session.successMessage = null;
+  req.session.errorMessage = null;
+
+  res.render('login', { successMessage, errorMessage });
+});
+
+router.post('/login', function(req, res, next) {
+  
+  res.redirect('/index');
+});
+
+router.post('/register', async function(req, res, next) {
+    const { username, password, email } = req.body;
+
+    let user = { username, password, email };
+    user.password = await bcrypt.hash(password, 10);
+
+    User.create(user)
+      .then((data) => {
+        req.session.successMessage = 'Registration successful! Please login.';
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    res.redirect('/login');
 });
 
 module.exports = router;
